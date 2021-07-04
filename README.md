1c9071317df19e8f07a4ca5796b9c8c2beec8711
Luke Sirand <lsirand@ucsd.edu>
2021-07-04 02:36:51 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
 # TwitterBot
+
+TwitterBot is a simple Node module that allows developers to build a Twitter bot via the Twitter API in conjunction with the [twit module](https://github.com/ttezel/twit). Using an object-oriented approach, TwitterBot makes calling functions more intuitive, resulting in cleaner and easier to understand code.
+
+TwitterBot arose from a personal need to encapsulate popular actions such as posting a status, retweeting a status, and searching for statuses, into a set of methods for better code maintainability and efficiency.
+
+## Instillation
+
+`npm i twitterbot --save`
+
+## Usage
+
+Be sure to first create a new bot (aka application) over at <https://developer.twitter.com/>. This package can and *should* be used in conjunction to <https://github.com/ttezel/twit>.
+
+### Features
+
+JSDoc highlighting with link to corresponding Twitter API reference:
+![highlight](images/highlighting.png)
+
+### Example Snippet
+
+```JavaScript
+const TwitterBot  = require('./TwitterBot');
+const MyBot = new TwitterBot(CONSUMER_KEY CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);
+
+MyBot.postTweet("Hello world!");  // Post a Tweet
+
+let params = {
+  attachment_url: 'https://github.com/sirandluke',
+}
+MyBot.postTweet("Check out my Github!", params);
+
+MyBot.searchTweets("oat milk latte").then(async (data) => {
+  let tweets = data.statuses;
+  for await (let tweet of tweets) {
+    let tweet_id = tweet.id;
+    MyBot.postRetweet(tweet_id);
+  }
+});
+
+MyBot.twit.stream('statuses/filter', { track: 'oolong milk tea' });
+```
+
+### Snippet Breakdown
+
+Import the module:
+
+```JavaScript
+const TwitterBot  = require('./TwitterBot');  // Import module
+```
+
+Initialize your bot object:
+
+```JavaScript
+const MyBot = new TwitterBot(CONSUMER_KEY CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);  // Initialize bot.
+```
+
+You can find find all your credentials over at <https://developer.twitter.com/>, under the `Projects & Apps` tab.
+
+Posting your first Tweet (aka status):
+
+```JavaScript
+const MyBot = new TwitterBot(CONSUMER_KEY CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);
+
+MyBot.postTweet("Hello world!");  // Post a Tweet
+```
+
+Posting a Tweet passing optional parameters:
+
+```JavaScript
+let params = {
+  attachment_url: 'https://github.com/sirandluke',
+}
+MyBot.postTweet("Check out my Github!", params);
+```
+
+Searching tweets based on a query and retweeting those tweets:
+
+```JavaScript
+MyBot.searchTweets("oat milk latte").then(async (data) => {
+  let tweets = data.statuses;
+  for await (let tweet of tweets) {
+    let tweet_id = tweet.id;
+    MyBot.postRetweet(tweet_id);
+  }
+});
+```
+
+Accessing the twit object to perform uncovered functions in the module:
+
+```JavaScript
+MyBot.twit.stream('statuses/filter', { track: 'oolong milk tea' })
+```
+
+## Contributing
+
+Feel free to make any changes and submit a pull request!
---
+++ TwitterBot.js
@@ -0,0 +1,152 @@
+const twit = require('twit');
+
+class TwitterBot {
+  /**
+   * Creates new twit object.
+   * @constructor
+   * 
+   * @class
+   * 
+   * @param  { string } consumer_key - Application's (bot) consumer key.
+   * @param  { string } consumer_secret - Application's consumer secret.
+   * @param  { string } access_token - Application's access token.
+   * @param  { string } access_token_secret - Application's token secret.
+   */
+  constructor(consumer_key, consumer_secret, access_token, access_token_secret) {
+    this.twit = new twit({
+      consumer_key: consumer_key,
+      consumer_secret: consumer_secret,
+      access_token: access_token,
+      access_token_secret: access_token_secret,
+    });
+  }
+
+  /**
+   * Updates the authenticating user's current status, also known as Tweeting.
+   * 
+   * @link  https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
+   * 
+   * @param  { string } status - The text of the status update. URL encode as necessary. t.co link wrapping will affect character counts.	
+   * @param  { JSON }   params - Additional optional parameters.
+   * 
+   * @return  { Promise } Returns response data or an error.
+   */
+  postTweet(status, params=null) {
+    return new Promise((resolve, reject) => {
+      if (params === null) {
+        params = {
+          status: status,
+        };
+      } else {
+        try {
+          params.status = status;
+        } catch(e) {
+          reject(e);
+        }
+      }
+      this.twit.post('statuses/update', params, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    });
+  }
+
+  /**
+   * (In development) Reply as a comment to a particular Tweet. Can omit in_reply_to_status_id in params.
+   * @param  { string } status
+   * @param  { string } id
+   * @param  { JSON }   params
+   * 
+   * @return  { Promise } 
+   */
+  _replyToTweet(status, id, params) {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  /**
+   * Returns a collection of relevant Tweets matching a specified query.
+   * 
+   * @link  https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
+   * 
+   * @param  { string } q - A UTF-8, URL-encoded search query of 500 characters maximum, including operators. Queries may additionally be limited by complexity.
+   * @param  { JSON }   params - Additional optional parameters.
+   * 
+   * @return  { Promise } Returns a list of statuses or an error.
+   */
+  searchTweets(q, params=null) {
+    return new Promise((resolve, reject) => {
+      if (params === null) {
+        params = {
+          q: q,
+        };
+      } else {
+        try {
+          params.q = q;
+        } catch(e) {
+          return reject(e);
+        }
+      }
+      this.twit.get('search/tweets', params, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    });
+  }
+
+  
+  /**
+   * Provides a simple, relevance-based search interface to public user accounts on Twitter.
+   * 
+   * @link  https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/follow-search-get-users/api-reference/get-users-search
+   * 
+   * @param  { string } q - The search query to run against people search.
+   * @param  { JSON }   params - Additional optional parameters
+   * 
+   * @returns  {Promise } Returns a list of users or an error.
+   */
+  searchUsers(q, params=null) {
+    return new Promise((resolve, reject) => {
+      if (params === null) {
+        params = {
+          q: q,
+        };
+      } else {
+        try {
+          params.q = q;
+        } catch(e) {
+          return reject(e);
+        }
+      }
+      this.twit.get('users/search', params, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    });
+  }
+
+  /**
+   * Retweets a tweet. Returns the original Tweet with Retweet details embedded (https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-retweet-id).
+   * @param  { string } id - The numerical ID of the desired status (see https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-retweet-id).
+   * @return  { Promise } Returns response data or an error.
+   */
+  postRetweet(id) {
+    return new Promise((resolve, reject) => {
+      twit.post('statuses/retweet/:id', id, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    });
+  }
+}
+
+module.exports = TwitterBot;
