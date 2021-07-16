a3a672c7466c56bb4192e528428f3e804ad71722
Luke Sirand <lsirand@ucsd.edu>
2021-07-16 16:56:59 -0700

    add basic operations such as posting and retweeting

--- TwitterBot.js
+++ TwitterBot.js
@@ -7,10 +7,13 @@ class TwitterBot {
    * Creates new twit object.
    * @constructor
    * 
-   * @param  { string } consumer_key - Application's (bot) consumer key.
-   * @param  { string } consumer_secret - Application's consumer secret.
-   * @param  { string } access_token - Application's access token.
-   * @param  { string } access_token_secret - Application's token secret.
+   * @param  {string} consumer_key - Application's (bot) consumer key.
+   * @param  {string} consumer_secret - Application's consumer secret.
+   * @param  {string} access_token - Application's access token.
+   * @param  {string} access_token_secret - Application's token secret.
+   * 
+   * @example 
+   * const MyBot = new TwitterBot('www', 'xxx', 'yyy', 'zzz');
    */
   constructor(consumer_key, consumer_secret, access_token, access_token_secret) {
     this.twit = new twit({
@@ -26,10 +29,13 @@ class TwitterBot {
    * 
    * @link  https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-update
    * 
-   * @param  { string } status - The text of the status update. URL encode as necessary. t.co link wrapping will affect character counts.	
-   * @param  { JSON }   params - Additional optional parameters.
+   * @param  {string} status - The text of the status update. URL encode as necessary. t.co link wrapping will affect character counts.	
+   * @param  {Object} [params] - Additional optional parameters.
    * 
-   * @return  { Promise } Returns response data or an error.
+   * @example
+   * MyBot.postTweet('Hello from Los Angeles!', {lat: 34.052235, long:  -​118.243683});
+   *
+   * @return  {Promise<Object>} Returns response data or an error.
    */
   postTweet(status, params=null) {
     return new Promise((resolve, reject) => {
@@ -41,7 +47,7 @@ class TwitterBot {
         try {
           params.status = status;
         } catch(e) {
-          reject(e);
+          return reject(e);
         }
       }
       this.twit.post('statuses/update', params, (err, data) => {
@@ -55,32 +61,55 @@ class TwitterBot {
 
 
   /**
-   * Post a Tweet containing a URL.
+   * Post a Tweet containing a Tweet permalink or direct message deep link.
    * 
    * @link  https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-update
+   * @readonly
+   * @param  {string} url -
+   * @param  {string} [status] -
+   * @param  {Object} [params] -
+   *
+   * @example
+   * MyBot.postLink('https://www.youtube.com/watch?v=Vp8fhW3Q4c8');
    * 
-   * @param  { string } url -
-   * @param  { string | null } status -
-   * @param  { JSON | null } params -
-   * 
-   * @return { Promise } Returns response data or an error.
+   * @return {Promise<Object>} Returns response data or an error.
    */
-  _postLink(url, status=null, params=null) {
-
+  postLink(url, status=null, params=null) {
+    return new Promise((resolve, reject) => {
+      if (params === null) {
+        params = {
+          status: status,
+          attachment_url: url,
+        }
+      } else {
+        try {
+          params.status = status;
+          params.attachment_url = url;
+        } catch (e) {
+          reject(e);
+        }
+      this.twit.post('statuses/update', params, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+      } 
+    });
   }
 
   /**
    * (In development) Reply as a comment to a particular Tweet. Can omit in_reply_to_status_id in params.
    * 
    * @link  https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-update
+   * @readonly
+   * @param  {string} id
+   * @param  {string} status
+   * @param  {Object} [params]
    * 
-   * @param  { string } status
-   * @param  { string } id
-   * @param  { JSON }   params
-   * 
-   * @return  { Promise } Returns response data or an error.
+   * @return  { Promise<Object> } Returns response data or an error.
    */
-  _replyToTweet(status, id, params=null) {
+  _replyToTweet(id, status, params=null) {
     return new Promise((resolve, reject) => {
 
     });
@@ -91,10 +120,13 @@ class TwitterBot {
    * 
    * @link  https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
    * 
-   * @param  { string } q - A UTF-8, URL-encoded search query of 500 characters maximum, including operators. Queries may additionally be limited by complexity.
-   * @param  { JSON }   params - Additional optional parameters.
+   * @param  {string} q - A UTF-8, URL-encoded search query of 500 characters maximum, including operators. Queries may additionally be limited by complexity.
+   * @param  {Object}   [params] - Additional optional parameters.
    * 
-   * @return  { Promise } Returns a list of statuses or an error.
+   * @example
+   * MyBot.searchTweets('oolong milk tea', {since_id: '1050118621198921700'});
+   * 
+   * @return  {Promise<Object>} Returns a list of statuses or an error.
    */
   searchTweets(q, params=null) {
     return new Promise((resolve, reject) => {
@@ -152,9 +184,17 @@ class TwitterBot {
   }
 
   /**
-   * Retweets a tweet. Returns the original Tweet with Retweet details embedded (https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-retweet-id).
-   * @param  { string } id - The numerical ID of the desired status (see https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-retweet-id).
+   * Retweets a tweet. Returns the original Tweet with Retweet details embedded.
+   * 
+   * @link https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-retweet-id
+   * 
+   * @param  { string } id - The numerical ID of the desired status.
    * @return  { Promise } Returns response data or an error.
+   * 
+   * @example
+   * MyBot.postRetweet('1050118621198921700');
+   * 
+   * @return  {Promise<Object>} Returns response data or an error.
    */
   postRetweet(id) {
     return new Promise((resolve, reject) => {
